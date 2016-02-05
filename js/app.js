7a574ef356d9ff113b2b2beff932d44f32db7eba
YeqingL <qq921027@hotmail.com>
2016-02-05 01:47:16 -0800

    edit contents

--- js/app.js
+++ js/app.js
@@ -1,75 +1,30 @@
-$(document).ready(function(){       
-    var scroll_pos = 0;
-    $(document).scroll(function() { 
-        scroll_pos = $(this).scrollTop();
-
-        // change background color
-        if(scroll_pos < 100) {
-            $("body").css('background-color', '#AFE8FF');
-        } else if(scroll_pos < 850) {
-            $("body").css('background-color', '#a4d2f4');
-        } else if(scroll_pos < 1200) {
-            $("body").css('background-color', '#9cc1ec');
-        } else {
-            $("body").css('background-color', '#91abe1');
-        }
-
-        // change banner display
-        if(scroll_pos < 400) {
-            $('#nav').fadeOut(100);
-        } else {
-            $('#nav').fadeIn();
-        }
-    });
-
-    $(function () {
-        $('#backToTop').click(function () {
-            $('body, html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-
-        $('#goToContact').click(function () {
-            $('body,html').animate({
-                scrollTop: $(document).height()
-            }, 800);
-            return false;
-        });
-
-        $('#goToAbout').click(function () {
-            $('body,html').animate({
-                scrollTop: 270
-            }, 800);
-            return false;
-        });
-
-        $('#goToWork').click(function () {
-            $('body,html').animate({
-                scrollTop: 900
-            }, 800);
-            return false;
-        });
-    });
-});
-
 var projects = angular.module('projects', ['ui.router'])
 projects.config(function($stateProvider, $urlRouterProvider) {
-    // $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/home');
 
     $stateProvider
-        .state('surf', {
+        .state('home', {
+            url:'/home',
+            templateUrl: 'templates/home.html'
+        })
+
+        .state('home.projects', {
+            url:'/projects',
+            templateUrl: 'templates/projects.html'
+        })
+
+        .state('home.projects.share', {
+            url:'/share',
+            templateUrl: 'templates/share.html'
+        })
+
+        .state('home.projects.surf', {
             url:'/surf',
             templateUrl: 'templates/surf.html'
         })
 
-        .state('aqua', {
+        .state('home.projects.aqua', {
             url:'/aqua',
             templateUrl: 'templates/aqua.html'
-        })
-
-        .state('share', {
-            url:'/share',
-            templateUrl: 'templates/share.html'
         })
 });
---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+
+        // change background color
+        if(scroll_pos < 100) {
+            $("body").css('background-color', '#AFE8FF');
+        } else if(scroll_pos < 850) {
+            $("body").css('background-color', '#a4d2f4');
+        } else if(scroll_pos < 1200) {
+            $("body").css('background-color', '#9cc1ec');
+        } else {
+            $("body").css('background-color', '#91abe1');
+        }
+
+        // change banner display
+        // if(scroll_pos < 400) {
+        //     $('#nav').fadeOut(100);
+        // } else {
+        //     $('#nav').fadeIn();
+        // }
+    });
+
+    $(function () {
+        $('#backToTop').click(function () {
+            $('body, html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        $('#goToContact').click(function () {
+            $('body,html').animate({
+                scrollTop: $(document).height()
+            }, 800);
+            return false;
+        });
+
+        $('#goToAbout').click(function () {
+            $('body,html').animate({
+                scrollTop: 270
+            }, 800);
+            return false;
+        });
+
+        $('#goToWork').click(function () {
+            $('body,html').animate({
+                scrollTop: 900
+            }, 800);
+            return false;
+        });
+    });
+});
