eb6c2c451044758876927f3837a827e498ebffdf
mattzhang105 <matt.zhang105@gmail.com>
2017-07-05 23:37:47 -0700

    Add Shared Navi Bar
    Add Shared Navi Bar

---
+++ js/navi.js
@@ -0,0 +1,12 @@
+ document.getElementById("navMenu").innerHTML =
+'<!-- <li><a ui-sref="projects.autelrobotics">Working @ Autel Robotics</a></li> -->'+
+'<li><a href="https://drive.google.com/file/d/0B93V9lskuMwDd2xSeGRWbUcyNlk/view?usp=sharing" target="_blank">WA Miles (Capstone Project)</a></li>'+
+'<li><a ui-sref="projects.aqua">Aqua Music Player</a></li>'+
+'<li><a ui-sref="projects.ambient">Ambient</a></li>'+
+'<li><a ui-sref="projects.surf">Surf Brand Redesign</a></li>'+
+'<hr>'+
+'<li><a ui-sref="projects.echo">How-To Helper</a></li>'+
+'<li><a href="http://depts.washington.edu/korcuban/#/home" target="_blank">Korean Cubans History</a></li>'+
+'<li><a ui-sref="projects.share">Share Web App</a></li>'+
+'<li><a ui-sref="projects.infoviz">Seattleites\' Travel Trend</a></li>'+
+'<li><a ui-sref="projects.more">Fun Time</a></li>';
